44e2c238cd5e78807fcb09f7e611210737f6f086
dannydelott <dannydelott@gmail.com>
2015-01-19 00:22:07 -0800

    Fix responsiveness
    - background-switcher.js -> add resize event handler
    - style.css -> fix video background resize dimensions

--- js/background-switcher.js
+++ js/background-switcher.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
-  
+
+  /* ******************
+   * Global Variables *
+   * ******************/
+
   var backgrounds = ['bg/coffee-beans-fast-fall-small.mp4',
                      'bg/coffee-beans-left-justified-small.mp4',
                      'bg/coffee-beans-small.mp4',
                      'bg/coffee-cup-small.mp4'];
-  
+  var $video = $('#bgvid'); 
   var $source = $('source');
+  
+  /* ***********
+   * Functions *
+   * ***********/
   
   var generateBackground = function(){
     return backgrounds[Math.floor(Math.random() * backgrounds.length)];
@@ -15,6 +23,21 @@ $(document).ready(function(){
     $source.attr('src', background);
   };
 
-  setBackground(generateBackground());
 
+  /* ****************
+   * Event Handlers *
+   * ****************/
+
+  $(window).on('resize', function() {
+
+    // shows/hides video background
+    if($(window).width() < 640){ $video.hide();}
+    else{ $video.show();}
+  });
+
+  /* **************
+   * Default code *
+   * **************/
+
+  setBackground(generateBackground());
 });
