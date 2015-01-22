5fbd23406e16bdb868ae6190a8ab9f27807ed357
dannydelott <dannydelott@gmail.com>
2015-01-21 16:06:15 -0800

    Add RSVP hover animation
    - lunchtime.html -> circle now "frosts" the background on hover.
    - rsvp.js -> contains jQuery hover event handler

---
+++ js/rsvp.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  
+  /* ******************
+   * Global Variables *
+   * ******************/
+
+  var $rsvp_frost = $('.circle.frost');
+  var $rsvp_circle = $('.circle.rsvp.fleft');
+
+  /* ****************
+   * Event Handlers *
+   * ****************/
+
+  // Frost the RSVP circle on hover
+  $rsvp_circle.hover(function(){
+   $rsvp_frost.fadeToggle(150);
+  });
+});
