151087271d5747680ad36561218443f81397c0df
Matthew Brown <matthewbrown@Matthews-MacBook-Pro.local>
2015-02-03 16:11:45 -0800

    Rework main.js
    Chagned code to be more functionalized and use less for-loops where
    possible

--- cloud/main.js
+++ cloud/main.js
@@ -2,230 +2,239 @@
 // Job to notify users of their match
 Parse.Cloud.job("notifyUsers", function(request, response) {
 
-  /*********************************
-   *   Matching Section
-   ********************************/
-
-var allUsers = [];
-var users = [];
-var matches = [];
-var rsvps = [];
-
-Parse.Cloud.useMasterKey();
-
-var updateMatches = function(callback) {
-
-  //pull all users
-  var Users = Parse.Object.extend('User');
-  var query= new Parse.Query(Users);
-
-
-  query.ascending('createdAt');
-  query.equalTo('channel', 'beta');
-  query.find({
-    success: function(userArray) {
-
-      //create name, match, rsvp arrays
-      for (var i = 0; i<userArray.length; i++) {
-        var object = userArray[i];
-        users.push(object.get('fullname'));
-        matches.push(object.get('matches'));
-        if (object.get('rsvp')){
-          rsvps.push(object.get('fullname'));
-        }
-      }
-      //For each user, Add new users to their match list
-      for(var j = 0; j<users.length; j++) {
-
-        //mactchListLength equals num key-value-pairs or 0 if undefined
-        var matchListLength = matches[j]?Object.keys(matches[j]).length:0;
-
-        //if total number of users > than this users matchlist + 1
-        //(add 1 to matchlist side because you don't include yourself in your own
-        //match list.
-        if ( users.length > matchListLength+1 ) {
-
-            //if matchListLength != 0, add 1 to it.
-            var k = matchListLength===0?0:matchListLength+1;
-
-            // add users, check to make sure we are not added ourself
-            for (k; k<users.length; k++) {
-
-              // dont add yourself
-              if (users[k] !== users[j]) {
-                matches[j][users[k]]=0;
-              }
-            }
-            // save to Parse
-            userArray[j].set('matches', matches[j]);
-            userArray[j].save();
-        }
-      }
-  }
-}).then(function(userArray){
+  Parse.Cloud.useMasterKey();
 
   // what we are returning overall, an array where each argument is an array of 3-4 user objects
   var dailyGroups = [];
+  var names = [];
+  var matches = [];
+  var rsvps = [];
 
-  var getUser = function(user) {
-    for (var i in userArray) {
-      if ( user === userArray[i].get('fullname') )
-        break;
+  var Users = Parse.Object.extend('User');
+  var query = new Parse.Query(Users);
+  query.ascending('createdAt');
+  query.equalTo('channel', 'beta');
+  query.find({ 
+    success : function(users) {
+      pullData(users);
+      updateUserMatches(users);
+      generateMatches(users);
+      uploadMatches(users);},
+      //emailUsers(users);},
+    error : function(users, err) {
+      console.log(JSON.stringify(err)); 
     }
-    return userArray[i];
-  };
-  var randomMatch = function () {
-    var matchNum = Math.floor(Math.random() * lowestCountList.length);
-    var match = lowestCountList[matchNum];
-    rsvps.splice(matchNum,1);
-    lowestCountList.splice(matchNum,1);
-    if ( lowestCountList.length <= 0 ) {
-      for (k; !matchesByCount.hasOwnProperty(k);  k++){}
-      lowestCountList = matchesByCount[k];
-    }
-    return match;
-  };
+  });
 
-  var incrementMatches = function(match1, match2) {
+  //}).then(function(users) { 
+  //  updateUserMatches(users);
+  //}).then(function(users) {
+  //  generateMatches(users);
+  //}).then(function(users) {
+  //  uploadMatches(users);
+  //).then(emailUsers(users));
+  //});
 
-    var matchObj = getUser(match1).get('matches');
-    matchObj[match2] = matchObj[match2] + 1;
-    getUser(match1).set('matches', matchObj);
-    getUser(match1).save();
-    incrementMatches(match2, match1);
-  };
+  function pullData(users) {
+    //create name, match, rsvp arrays
+    users.forEach(function(user, index, array) {
+      names.push(user.get('fullname'));
+      matches.push(user.get('matches'));
+      if (user.get('rsvp')){
+        rsvps.push(user.get('fullname'));
+      }
+    });
+  }
+    
+  function updateUserMatches(users) {
+    console.log('in updateUserMatches');
+    // updates each users' matches object 
+    users.forEach(function(user,index,array){
+      var numMatches = Object.keys(matches[index]).length;
+      // if new users need to be added
+      if ( names.length > numMatches+1 ) {
+        numMatches = numMatches===0 ? 0 : numMatches+1;
 
-  // repeat code until we have matched everyone who RSVPed
-  while (rsvps.length > 0) {
-
-    var currentRSVP = rsvps.shift();
-    var currentMatches = {};
-
-    //match first person in rsvp list with their User name
-    for(var i = 0; i<users.length; i++) {
-        if (currentRSVP  === users[i]) {
-          currentMatches = matches[i];
-          break;
+        for (numMatches; numMatches < names.length; numMatches++) {
+          if (names[numMatches] !== names[index]) {
+            matches[index][names[numMatches]]=0;
+          }
         }
+        // save to Parse
+        users[index].set('matches', matches[index]);
+        users[index].save();
+      }       
+    });
+  }
+
+  function generateMatches(users) { 
+      
+    // increments the matches of the users.
+    var incrementMatches = function(user1, user2, stop) {
+      var matchObj = getUser(user1).get('matches');
+      matchObj[user2] = matchObj[user2] + 1;
+      getUser(user1).set('matches', matchObj);
+      getUser(user1).save();
+    
+      if (!stop) { 
+        incrementMatches(user2, user1, true);
+      }
+    };
+
+    while (rsvps.length > 0) {
+  
+      var currentUser = rsvps.splice(Math.floor(Math.random() * rsvps.length),1).shift();
+      var currentMatchesByCount = new matchesByCount();
+
+      // make random pairs until divisible by 3, then make groups of 3
+      var match1 = currentMatchesByCount.randomMatch();
+      if ( (rsvps.length + 2)%3 === 0 ) {
+        var match2 = currentMatchesByCount.randomMatch();
+        //update each users matchlist
+        incrementMatches(match2, match1);
+        incrementMatches(match2, currentUser);
+        //add to daily group array
+        dailyGroups.push([getUser(currentUser),getUser(match1),getUser(match2)]);
+        console.log(currentUser+ ' was matched with ' + match1 + ' and ' + match2);
+      } else {
+        //add to daily group array
+        dailyGroups.push([getUser(currentUser),getUser(match1)]);
+        console.log(currentUser + ' was matched with ' + match1);
+      }
+        incrementMatches(currentUser, match1);
     }
 
-    //make an rsvp list for this person
-    var matchesByName = {};
-    for(var ii = 0; ii<rsvps.length; ii++) {
-        for (var jj in currentMatches){
-          if ( rsvps[ii] === jj ) {
-            matchesByName[jj] = currentMatches[jj];
+    function matchesByCount() {
+      var counts = getMatchesByCount();
+      var keys = Object.keys(counts).sort( function(a,b) {return a-b;}); 
+      var keyIndex = 0;
+
+      function randomMatch () {
+        // get the key for the array of names with lowest number of matches
+        getLowestKeyVal();
+
+        // match randomly from array 
+        console.log('currentUser: ' + currentUser);
+        console.log('keys: ' + keys + ', keyIndex: ' + keyIndex);
+        console.log('counts[keys[keyIndex]]: ' + counts[keys[keyIndex]]);
+        var matchNum = Math.floor(Math.random() * counts[keys[keyIndex]].length);
+        console.log('matchNum: ' + matchNum);
+        var match = counts[keys[keyIndex]][matchNum];
+
+        // remove from RSVP array
+        var index = rsvps.indexOf(match);
+        rsvps.splice(index,1);
+          
+        // remove from counts array
+        counts[keys[keyIndex]].splice(matchNum,1);
+
+        return match;
+      }// randomMatch
+
+      function getMatchesByCount() {
+        var matchesByName = getMatchesByName();
+        var matchesByCount = {};
+        for(var name in matchesByName) {
+          if( matchesByCount.hasOwnProperty(matchesByName[name]) ){
+            matchesByCount[matchesByName[name]].push(name);
+          }else{
+            matchesByCount[matchesByName[name]] = [name];
+          }
+        }
+        return matchesByCount;
+      }// getMatchesByCount
+
+      function getMatchesByName() {
+        var currentUserMatches = {};
+        for (var i = 0; i<users.length; i++) {
+          if ( currentUser === users[i].get('fullname') ) {
+            currentUserMatches = users[i].get('matches');
             break;
           }
         }
-    }
+        var matchesByName = {};
+        rsvps.forEach( function(rsvp) {
+          for (var match in currentUserMatches) {
+            if (rsvp === match){
+              matchesByName[rsvp] = currentUserMatches[match];
+              break;
+            }
+          }
+        });
+        return matchesByName;
+      }// getMatchesByName
 
-    //swap key-value-pairs (keys become values and vice versa)
-    var matchesByCount = {};
-    for(var j in matchesByName) {
-      if( matchesByCount.hasOwnProperty(matchesByName[j]) ){
-        matchesByCount[matchesByName[j]].push(j);
-      }else{
-        matchesByCount[matchesByName[j]] = [j];
-      }
-    }
+      function getLowestKeyVal() {
+        if ( counts[keys[keyIndex]].length === 0 ) {
+            keyIndex += 1;
+        }
+      }// getLowestKeyVal
+      return {randomMatch:randomMatch};
 
-    //get the list of names with lowest number of matches
-    var k = 0;
-    for (k; !matchesByCount.hasOwnProperty(k);  k++){}
-    var lowestCountList = matchesByCount[k];
+    }// matchesByCount
+ 
+    // getUser returns the User object if matched with a fullname
+    function getUser(fullname) {
+      var result = {};
+      users.forEach( function(user, index, array) {
+        if ( fullname === user.get('fullname') ) {
+          result = user;
+        }
+      });
+      return result;
+    }// getUser
 
-
-    //create a matches randomly
-    var match1 = randomMatch();
-
-    if (rsvps.length%3 === 0) {
-      var match2 = randomMatch();
-
-      //update each users matchlist
-      incrementMatches(match2, match1);
-      incrementMatches(match2, currentRSVP);
-      //add to daily group array
-      dailyGroups.push([getUser(currentRSVP),getUser(match1),getUser(match2)]);
-      console.log(currentRSVP + ' was matched with ' + match1 + ' and ' + match2);
-    } else {
-      //add to daily group array
-      dailyGroups.push([getUser(currentRSVP),getUser(match1)]);
-      console.log(currentRSVP + ' was matched with ' + match1);
-      incrementMatches(currentRSVP, match1);
-    }
-  }
-
-  uploadMatches(dailyGroups);
-  notifyMatches(dailyGroups);
-});
-
+ }// generateMatches 
+  
   // Upload matches to the Matches class on Parse
-  var uploadMatches = function(matchGroup) {
+  function uploadMatches() {
     var Matches = Parse.Object.extend('Matches');
     var matchArray = new Matches();
-
-    // console.log('Uploading matches..');
-
-    matchArray.save({matches: matchGroup});
-  };
-
-  /*********************************
-   *   Email Section
-   ********************************/
-
- // Notify a 2d array of matches
- var notifyMatches = function(matchGroup) {
-
-   // Set up the SendGrid parameters
-   var sendgrid = require("sendgrid");
-   sendgrid.initialize("LunchReactor", "hackreactor0");
-
-   // Send a notification email to an array of users
-   var notifyUsers = function(userGroup) {
-     var userEmails = [];
-     var userDetails = 'Check out your group for today\'s lunch!<br><br><br>';
-
-     // Set email recipients and their message content
-     for (var j = 0; j < userGroup.length; j++) {
-       userEmails.push(userGroup[j].get('email'));
-       userDetails += '<b>' + userGroup[j].get('fullname') + '</b><br>' +
-                      userGroup[j].get('email') + '<br>' +
-                      userGroup[j].get('signature') + '<br><br>';
-     }
-
-    //  console.log('Sending emails to ' + userEmails.join(', '));
-     // Call the SendGrid api to send the emails
-     sendgrid.sendEmail({
-       to: userEmails,
-       from: 'lunchreactor@gmail.com',
-       subject: 'View Today\'s Lunch Match!',
-       html: userDetails + '<br>'
-     }, {
-       success: function(httpResponse) {
-         console.log(httpResponse);
-         response.success("Email sent!");
-       },
-       error: function(httpResponse) {
-         console.error(httpResponse);
-         response.error("Uh oh, something went wrong");
-       }
-     });
-   };
-
-   // Loop through the matches to notify every set of users
-   for (var i = 0; i < matchGroup.length; i++) {
-    //  console.log('Notifying Group ' + i);
-     notifyUsers(matchGroup[i]);
-   }
- };
-
-};
-
-
-  console.log("Starting");
-  //getMatches();
-  updateMatches();
+    matchArray.save({matches: dailyGroups});
+  }
+  
+  function emailUsers() {
+    // Set up the SendGrid parameters
+    var sendgrid = require("sendgrid");
+    sendgrid.initialize("LunchReactor", "hackreactor0");
+  
+    // Send a notification email to an array of users
+    var notifyUsers = function(userGroup) {
+      var userEmails = [];
+      var userDetails = 'Check out your group for today\'s lunch!<br><br><br>';
+  
+      // Set email recipients and their message content
+      for (var j = 0; j < userGroup.length; j++) {
+        userEmails.push(userGroup[j].get('email'));
+        userDetails += '<b>' + userGroup[j].get('fullname') + '</b><br>' +
+                               userGroup[j].get('email') + '<br>' +
+                               userGroup[j].get('signature') + '<br><br>';
+      }
+  
+      //  console.log('Sending emails to ' + userEmails.join(', '));
+      // Call the SendGrid api to send the emails
+      sendgrid.sendEmail({
+        to: userEmails,
+        from: 'lunchreactor@gmail.com',
+        subject: 'View Today\'s Lunch Match!',
+        html: userDetails + '<br>'
+        }, {
+        success: function(httpResponse) {
+          console.log(httpResponse);
+          response.success("Email sent!");
+        },
+        error: function(httpResponse) {
+          console.error(httpResponse);
+          response.error("Uh oh, something went wrong");
+        }
+      });
+    };
+  
+    // Loop through the matches to notify every set of users
+    for (var i = 0; i < dailyGroups.length; i++) {
+      notifyUsers(dailyGroups[i]);
+    }
+  }
 
 });
 
